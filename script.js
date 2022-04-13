8330d0c69b6cbaf9a219b76c318adc56c54ab729
Prosthius <ietvntpd@duck.com>
2022-04-13 17:53:06 +1000

    Working on playRound() function

--- script.js
+++ script.js
@@ -1,9 +1,34 @@
-computerPlay();
+playRound();
 
 function computerPlay()
 {
     const rockPaperScissors = ["Rock", "Paper", "Scissors"];
 
     const rnd = Math.floor(Math.random() * rockPaperScissors.length);
-    return console.log(rnd, rockPaperScissors[rnd]);
+    let random = '';
+    random = (rnd, rockPaperScissors[rnd]);
+    return random;
+}
+
+function playRound(playerSelection, computerSelection)
+{
+    playerSelection = prompt()
+    playerSelection = playerSelection[0].toUpperCase() + playerSelection.substring(1);
+    computerSelection = computerPlay().toUpperCase() + computerPlay().substring(1);
+    //console.log(playerSelection);
+    //console.log(computerSelection);
+
+    if (playerSelection === computerSelection)
+    {
+        console.log("It's a tie.")
+    } else if (
+        (playerSelection === 'rock' && computerSelection === 'scissors') ||
+        (playerSelection === 'paper' && computerSelection === 'rock') ||
+        (playerSelection === 'scissors' && computerSelection === 'paper'))
+    {
+        console.log(`You win!\n${playerSelection} beats ${computerSelection}.`)
+    } else 
+    {
+        console.log(`You lose!\n${computerSelection} beats ${playerSelection}.`)
+    }
 }
